fc43ba931bff1bd9586223c028b1c5c18f79df5e
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2024-12-16 06:09:41 -0300

    Create figure with three metricsfor chest strap.

--- ccc_barplot.py
+++ ccc_barplot.py
@@ -8,6 +8,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+Metrics = ['HRV_MeanNN', 'HRV_SDNN', 'HRV_RMSSD', 'HRV_SDSD', 'HRV_CVSD',
+           'HRV_CVNN', 'HRV_TINN', 'HRV_HTI', 'HRV_SDRMSSD', 'HRV_pNN20',
+           'HRV_pNN50', 'HRV_IQRNN', 'HRV_LF', 'HRV_HF', 'HRV_LFHF',
+           'HRV_LFn', 'HRV_HFn', 'HRV_LnHF', 'HRV_SD1', 'HRV_SD2',
+           'HRV_SD1SD2', 'HRV_SampEn', 'HRV_TP']
+
 # TODO: Import from multipage_pdf
 from matplotlib.backends.backend_pdf import PdfPages
 def save_figs_as_pdf(figs, fn):
@@ -18,25 +24,18 @@ def save_figs_as_pdf(figs, fn):
         pdf.close()
     else:
         figs.savefig(fn, format='pdf')
-    print('File %s created' % fn)
-
-metrics = ['HRV_MeanNN', 'HRV_SDNN', 'HRV_RMSSD', 'HRV_SDSD', 'HRV_CVSD',
-           'HRV_CVNN', 'HRV_TINN', 'HRV_HTI', 'HRV_SDRMSSD', 'HRV_pNN20',
-           'HRV_pNN50', 'HRV_IQRNN', 'HRV_LF', 'HRV_HF', 'HRV_LFHF',
-           'HRV_LFn', 'HRV_HFn', 'HRV_LnHF', 'HRV_SD1', 'HRV_SD2',
-           'HRV_SD1SD2', 'HRV_SampEn', 'HRV_TP']
+    print('Figure saved as', fn)
 
 
-def make_barplot(fn, methods, experiments):
-    df = pd.read_csv(fn)
-    x = np.arange(len(experiments))  # the label locations
+def make_barplot(df, methods, experiments, axs, metrics=None, ytitle=0.89):
+    if metrics is None:
+        metrics = Metrics
     if len(methods) < 8:
         width = 0.12 # the width of the bars
     else:
         width = 0.1
-    rows, cols = 4, 6
-    fig, axs = plt.subplots(rows, cols, layout='constrained', figsize=(19.2, 9.8))
 
+    x = np.arange(len(experiments))  # the label locations
     for metric, ax in zip(metrics, axs.flat):
         dfm = df[df['metric'] == metric]
         # TODO: Sort the rows of `dfm` to match `experiments`
@@ -44,13 +43,12 @@ def make_barplot(fn, methods, experiments):
         for method in methods:
             data = dfm[dfm['method'] == method]['ccc'].values
             offset = width * multiplier
-            rects = ax.bar(x + offset, data, width, label=method)
+            ax.bar(x + offset, data, width, label=method)
             multiplier += 1
 
         ax.set_xticks(x + 3*width, experiments)
         ax.set_ylim(0, 1.1)
-        ax.set_title(metric.split('_')[1], y=0.89)
-
+        ax.set_title(metric.split('_')[1], y=ytitle)
         # See https://pmc.ncbi.nlm.nih.gov/articles/PMC6107969/, which recommend
         # the values from
         # Chan, Y. H. "Biostatistics 104: correlational analysis." Singapore Med
@@ -59,6 +57,14 @@ def make_barplot(fn, methods, experiments):
         ax.axhline(0.5, color='black', linestyle=':', alpha=0.5)
         ax.axhline(0.3, color='black', linestyle=':', alpha=0.5)
 
+    return x, width
+
+
+def make_barplots(fn, methods, experiments):
+    df = pd.read_csv(fn)
+    rows, cols = 4, 6
+    fig, axs = plt.subplots(rows, cols, layout='constrained', figsize=(19.2, 9.8))
+    x, width = make_barplot(df, methods, experiments, axs)
 
     for i in range(rows):
         for j in range(cols):
@@ -80,24 +86,56 @@ def make_barplot(fn, methods, experiments):
     return fig
 
 
-if __name__ == '__main__':
+def make_all_plots():
     plt.close('all')
+
     methods = ['Elgendi_et_al', 'Matched_filter', 'Wavelet_transform', 'Christov',
            'Hamilton', 'Pan_Tompkins', 'WQRS']
-    experiments = ['sitting', 'maths', 'walking', 'hand_bike', 'jogging']
     experiments = ['sitting', 'maths', 'walking', 'handbk', 'jogging']
 
     figs = []
-    fig = make_barplot('datahrv/ccc_chest_strap_df.csv', methods, experiments)
+    fig = make_barplots('datahrv/ccc_chest_strap_df.csv', methods, experiments)
     figs.append(fig)
 
 
     methods = ['Elgendi_et_al', 'Matched_filter', 'Wavelet_transform', 'Engzee',
                'Christov', 'Hamilton', 'Pan_Tompkins', 'WQRS']
     experiments = ['sitting', 'maths', 'walking', 'handbk']
-    fig = make_barplot('datahrv/ccc_einhoven_df.csv', methods, experiments)
+    fig = make_barplots('datahrv/ccc_einhoven_df.csv', methods, experiments)
     figs.append(fig)
 
     plt.show()
 
-    save_figs_as_pdf(figs, 'figures/ccc_barplot.pdf')
+    save_figs_as_pdf(figs, 'figures/ccc_barplots.pdf')
+
+
+
+def make_paper_plot():
+    fn = 'datahrv/ccc_chest_strap_df.csv'
+    df = pd.read_csv(fn)
+    plt.close('all')
+    fig, axs = plt.subplots(4, 1, layout='constrained', figsize=(5, 7))
+    methods = ['Elgendi_et_al', 'Matched_filter', 'Wavelet_transform', 'Christov',
+               'Hamilton', 'Pan_Tompkins', 'WQRS']
+    experiments = ['sitting', 'maths', 'walking', 'handbk', 'jogging']
+
+    metrics = ['HRV_MeanNN', 'HRV_TINN', 'HRV_LFHF']
+    x, width = make_barplot(df, methods, experiments, axs, metrics, ytitle=0.95)
+    for ax in axs:
+        ax.set_ylabel('CCC')
+        # ax.set_ylim(ymax=1.2)
+
+    legend_panel = axs[-1]
+    legend_panel.axis('off')
+    legend_panel.legend(handles=axs[0].get_legend_handles_labels()[0],
+                       labels=axs[0].get_legend_handles_labels()[1],
+                        loc="center", ncols=len(methods)//2)
+    fn = 'figures/ccc_barplot.pdf'
+    plt.savefig(fn)
+    print('Figure saved as', fn)
+    plt.show()
+
+
+if __name__ == '__main__':
+    make_paper_plot() # Figure with three metrics for chest strap.
+    make_all_plots()  # Figure with all metrics and the two setups.
