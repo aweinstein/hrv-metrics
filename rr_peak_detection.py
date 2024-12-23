9301af1c4924626b29e2e105f0622aef430a5779
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2024-12-23 13:54:46 -0300

    Remove unused plotting code.

--- rr_peak_detection.py
+++ rr_peak_detection.py
@@ -16,14 +16,13 @@ from jf.sensitivity_analysis import evaluate as sens
 from utils import save_figs_as_pdf
 
 
-PLOT = False
 SAVE_PATH = Path('results/rr_detection')
 
-NSUBJECTS = 24
+n_subjects = 24
 FS = 250
 DETECTOR = 'chest_strap'
 experiments = ['sitting', 'maths', 'walking', 'hand_bike', 'jogging']
-subjects = np.arange(0, NSUBJECTS+1)
+subjects = np.arange(0, n_subjects + 1)
 
 
 methods_names = ['Elgendi_et_al', 'Matched_filter', 'Wavelet_transform',
@@ -106,89 +105,18 @@ for s in tqdm(subjects, desc='Subject'):
 
                 JF.append(jf(detected_peaks, annotated_peaks, FS, None)['jf'])
 
-            # Plot 5s if wanted
-            if PLOT:
-                # Deffine plotting window
-                T1, T2 = 10, 15
-                fig = plt.figure()
-                data_range = [np.amin(data), np.amax(data)]
-                plt.plot(time, data, color='teal')
-
-                plt.scatter(time[annotated_peaks], data[annotated_peaks],
-                            color='red', label='Annotated', marker='x', alpha=0.7)
-                plt.scatter(time[detected_peaks], data[detected_peaks],
-                            color='blue', label='Detected', marker='o', s=50, alpha=0.5)
-
-                line1 = [plt.axvline(x)
-                         for x in ((annotated_peaks - W)/FS)]
-                line2 = [plt.axvline(x)
-                         for x in ((annotated_peaks + W)/FS)]
-
-                # Identify timepoints inside the windows
-                no_fill = np.any(
-                    np.abs(np.arange(len(time))[:, None] -
-                           annotated_peaks[None, :]) < W,
-                    axis=1)
-
-                plt.fill_between(time,
-                                 data-np.amax(data)*0.1,
-                                 data+np.amin(data)*0.1,
-                                 where=no_fill,
-                                 alpha=0.3,
-                                 color='green')
-
-                plt.xlabel('Time [s]')
-                plt.ylabel('Voltage')
-                plt.legend()
-
-                plt.xlim([T1, T2])
-                # y_lim = [np.amin(data[np.argmin(abs(T1-time)):np.argmin(abs(T2-time))])*1.1,
-                #         np.amax(data[np.argmin(abs(T1-time)):np.argmin(abs(T2-time))])*0.9]
-
-                plt.title(f'Method: {method[0]}\nExperiment: {experiment}')
-
-                # plt.ylim(y_lim)
-
-                plt.tight_layout()
-
-                fig.savefig(
-                    f'{SAVE_PATH}/s[{s}]/{experiment}/{methods_names[i]}/5s.png')  # png
-                fig.savefig(
-                    f'{SAVE_PATH}/s[{s}]/{experiment}/{methods_names[i]}/5s.pdf')  # pdf
-                figs.append(fig)
-                # plt.close()
-
-                # Plot Histogram
-                fig2 = plt.figure(figsize=(7, 4))
-
-                plt.hist(np.diff(detected_peaks / FS), bins=30, color='purple')
-                plt.xlabel('Time [s]')
-                plt.title(f'Subject: {s}\n Method: {method}')
-                fig2.savefig(
-                    f'{SAVE_PATH}/s[{s}]/{experiment}/{methods_names[i]}/histogram.png')  # png
-                fig2.savefig(
-                    f'{SAVE_PATH}/s[{s}]/{experiment}/{methods_names[i]}/histogram.pdf')  # pdf
-
-            if PLOT:
-                save_figs_as_pdf(figs,
-                                 f'{SAVE_PATH}/s[{s}]/{experiment}/time_series.pdf')
-                plt.close('all')
-# %% Arrange in to pandas array
-
+# Arrange into pandas array
 data_dict = {'sensitivity': np.array(sensitivity),
              'JF': np.array(JF)*100,
              'method': np.array(methods_name),
              'experiment': np.array(experiments_name),
              'subject_idx': np.array(subject_idx)}
-
 data = pd.DataFrame.from_dict(data_dict)
-
 core_df = data.to_csv('results/core_df.csv')
-
 data.to_csv(SAVE_PATH / 'sensitivity_jf.csv')
 
-# %% Sensitivity Plot
-
+# Sensitivity Plot
+plt.close('all')
 fig, ax = plt.subplots(figsize=(7, 4))
 sns.barplot(data=data, x='method', y='sensitivity',
             hue='experiment', errorbar='sd', ax=ax)
