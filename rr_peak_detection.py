be867a05a32ab50913ce98b7287d7af49c9fda87
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2024-12-23 15:34:21 -0300

    Transform code into a function to compute both setups in one script.

--- rr_peak_detection.py
+++ rr_peak_detection.py
@@ -17,102 +17,107 @@ from utils import subjects, experiments, methods_names
 
 save_path = Path('results/rr_detection')
 fs = 250
-setup = 'chest_strap'
-
 # %% Initialize Porr detectors
 detectors = Detectors(fs)
 methods = detectors.get_detector_list()
 
-# Allocate arrays that later compose the DF
-sensitivity = []
-JF = []
-methods_name = []
-experiments_name = []
-subject_idx = []
 
-for s in tqdm(subjects, desc='Subject'):
-    for experiment in tqdm(experiments, desc='Condition'):
-        ecg_class = GUDb(s, experiment)
+def detect_peaks(setup):
+    # Allocate arrays that later compose the DF
+    sensitivity = []
+    JF = []
+    methods_name = []
+    experiments_name = []
+    subject_idx = []
 
-        # Anotated R-peaks and data
-        if setup == 'Einhoven':
-            anno_exists = ecg_class.anno_cables_exists
-            annotated_peaks = ecg_class.anno_cables
-            data = ecg_class.einthoven_II
+    for s in tqdm(subjects, desc='Subject'):
+        for experiment in tqdm(experiments, desc='Condition'):
+            ecg_class = GUDb(s, experiment)
 
-        elif setup == 'chest_strap':
-            anno_exists = ecg_class.anno_cs_exists
-            annotated_peaks = ecg_class.anno_cs
-            data = ecg_class.cs_V2_V1
+            # Anotated R-peaks and data
+            if setup == 'einthoven':
+                anno_exists = ecg_class.anno_cables_exists
+                annotated_peaks = ecg_class.anno_cables
+                data = ecg_class.einthoven_II
 
-        if not anno_exists:
-            tqdm.write(f'No annotation for subject {s}, condition {experiment}. '
-                       'Skipping it.')
-            continue
+            elif setup == 'chest_strap':
+                anno_exists = ecg_class.anno_cs_exists
+                annotated_peaks = ecg_class.anno_cs
+                data = ecg_class.cs_V2_V1
 
-        time = ecg_class.t
-        fs = ecg_class.fs
+            if not anno_exists:
+                tqdm.write(f'No annotation for subject {s}, condition {experiment}. '
+                           'Skipping it.')
+                continue
 
-        # tolerance window in samples
-        W = int(fs / 10)
+            time = ecg_class.t
+            fs = ecg_class.fs
 
-        fn = save_path / f'{s:02d}_{setup}_{experiment}_annotated_peaks.npy'
-        np.save(fn, annotated_peaks)
+            # tolerance window in samples
+            W = int(fs / 10)
 
-        figs = []
-        for i, method in enumerate(tqdm(methods, desc='Method', leave=False)):
+            fn = save_path / f'{s:02d}_{setup}_{experiment}_annotated_peaks.npy'
+            np.save(fn, annotated_peaks)
 
-            # Find peaks
-            detected_peaks = np.array(method[1](data))
+            figs = []
+            for i, method in enumerate(tqdm(methods, desc='Method', leave=False)):
 
-            fn = f'{s:02d}_{setup}_{experiment}_{methods_names[i]}_peaks.npy'
-            fn = save_path / fn
+                # Find peaks
+                detected_peaks = np.array(method[1](data))
 
-            # Save detected peaks
-            np.save(fn, detected_peaks)
+                fn = f'{s:02d}_{setup}_{experiment}_{methods_names[i]}_peaks.npy'
+                fn = save_path / fn
 
-            # Compute sensitivity
-            if len(detected_peaks) > 10:
+                # Save detected peaks
+                np.save(fn, detected_peaks)
 
-                experiments_name.append(experiment)
-                methods_name.append(methods_names[i])
-                subject_idx.append(s)
+                # Compute sensitivity
+                if len(detected_peaks) > 10:
 
-                results = sens(detected_peaks, annotated_peaks, W)
-                sensitivity.append(results[0])
+                    experiments_name.append(experiment)
+                    methods_name.append(methods_names[i])
+                    subject_idx.append(s)
 
-                JF.append(jf(detected_peaks, annotated_peaks, fs, None)['jf'])
+                    results = sens(detected_peaks, annotated_peaks, W)
+                    sensitivity.append(results[0])
 
-# Arrange into pandas array
-data_dict = {'sensitivity': np.array(sensitivity),
-             'JF': np.array(JF)*100,
-             'method': np.array(methods_name),
-             'experiment': np.array(experiments_name),
-             'subject_idx': np.array(subject_idx)}
-data = pd.DataFrame.from_dict(data_dict)
-core_df = data.to_csv('results/core_df.csv')
-data.to_csv(save_path / 'sensitivity_jf.csv')
+                    JF.append(jf(detected_peaks, annotated_peaks, fs, None)['jf'])
 
-# Sensitivity and JF plots
-plt.close('all')
-fig, ax = plt.subplots(figsize=(7, 4))
-sns.barplot(data=data, x='method', y='sensitivity',
-            hue='experiment', errorbar='sd', ax=ax)
-plt.setp(ax.xaxis.get_majorticklabels(), rotation=-
-         45, ha="left", rotation_mode="anchor")
-plt.ylabel('Sensitivity [%]')
-plt.xlabel('Method')
-plt.title(setup)
-plt.tight_layout()
-fig.savefig(save_path / f'sensitivity_{setup}.pdf')
+    # Arrange into pandas array
+    data_dict = {'sensitivity': np.array(sensitivity),
+                 'JF': np.array(JF)*100,
+                 'method': np.array(methods_name),
+                 'experiment': np.array(experiments_name),
+                 'subject_idx': np.array(subject_idx)}
+    data = pd.DataFrame.from_dict(data_dict)
+    core_df = data.to_csv('results/core_df.csv')
+    data.to_csv(save_path / 'sensitivity_jf.csv')
 
-fig, ax = plt.subplots(figsize=(7, 4))
-sns.barplot(data=data, x='method', y='JF', hue='experiment', errorbar='sd',
-            ax=ax)
-plt.setp(ax.xaxis.get_majorticklabels(), rotation=-
-         45, ha="left", rotation_mode="anchor")
-plt.ylabel('JF [%]')
-plt.xlabel('Method')
-plt.title(setup)
-plt.tight_layout()
-fig.savefig(save_path / f'JF_{setup}.pdf')
+    # Sensitivity and JF plots
+    plt.close('all')
+    fig, ax = plt.subplots(figsize=(7, 4))
+    sns.barplot(data=data, x='method', y='sensitivity',
+                hue='experiment', errorbar='sd', ax=ax)
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=-
+             45, ha="left", rotation_mode="anchor")
+    plt.ylabel('Sensitivity [%]')
+    plt.xlabel('Method')
+    plt.title(setup)
+    plt.tight_layout()
+    fig.savefig(save_path / f'sensitivity_{setup}.pdf')
+
+    fig, ax = plt.subplots(figsize=(7, 4))
+    sns.barplot(data=data, x='method', y='JF', hue='experiment', errorbar='sd',
+                ax=ax)
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=-
+             45, ha="left", rotation_mode="anchor")
+    plt.ylabel('JF [%]')
+    plt.xlabel('Method')
+    plt.title(setup)
+    plt.tight_layout()
+    fig.savefig(save_path / f'JF_{setup}.pdf')
+
+
+if __name__ == '__main__':
+    detect_peaks('chest_strap')
+    detect_peaks('einthoven')
