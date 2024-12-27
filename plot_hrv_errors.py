de5a0f1d64409f331bd53c5b79f50441c78ec316
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2024-12-27 19:11:15 -0300

    Add function to plot all the HRV metrics and its error.

---
+++ plot_hrv_errors.py
@@ -0,0 +1,52 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+from utils import read_info, plot_hrv, save_figs_as_pdf
+
+# %% Read info file
+info = read_info()
+core_df = pd.read_csv('results/core_df.csv')
+# subject_list = np.unique(core_df['subject_idx'])
+experiments = info['experiments']
+methods_names = np.array(info['methods_names'])
+methods_names = methods_names[methods_names != 'Engzee']
+setup = 'einthoven'
+FS = 250  # This should be inside the info file
+
+setup = 'einthoven'
+df_det_hrv = pd.read_csv(f'results/HRV/{setup}_HRV_results.csv')
+t_df = df_det_hrv[df_det_hrv.method != 'Annotated']
+an_df = df_det_hrv[df_det_hrv.method == 'Annotated']
+
+ann_hrv_arr = an_df.to_numpy().repeat(len(methods_names),
+                                      axis=0)[:, :-3]
+error = abs(t_df[t_df.columns[:-3]] - ann_hrv_arr)
+error_df = pd.concat([t_df[t_df.columns[-3:]], error], axis=1)
+hrvs = df_det_hrv.columns.tolist()
+hrvs.remove('subject_idx')
+hrvs.remove('experiment')
+hrvs.remove('method')
+hrvs.remove('Unnamed: 0')
+
+plt.close('all')
+figs = []
+for y in tqdm(hrvs):
+    fig, ax = plot_hrv(df_det_hrv, x='experiment', y=y, hue='method')
+    fig.tight_layout()
+    figs.append(fig)
+    plt.close(fig)
+fn = f'results/HRV/plot_hrv_{setup}.pdf'
+print(f'Saving plots into {fn}...')
+save_figs_as_pdf(figs, fn)
+
+figs = []
+for y in tqdm(hrvs):
+    fig, ax = plot_hrv(error_df, x='experiment', y=y, hue='method')
+    fig.tight_layout()
+    figs.append(fig)
+    plt.close(fig)
+fn = f'results/HRV/plot_hrv_error_{setup}.pdf'
+print(f'Saving plots into {fn}...')
+save_figs_as_pdf(figs, fn)
