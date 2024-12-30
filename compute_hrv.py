740871b73939b99c68d3dc436f13c86d25e5be10
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2024-12-30 11:19:57 -0300

    Transform code into a function to compute both setups in one script.

--- compute_hrv.py
+++ compute_hrv.py
@@ -7,62 +7,66 @@ import numpy as np
 import pandas as pd
 import neurokit2 as nk
 from tqdm import tqdm
-from utils import read_info, plot_hrv, make_peaks_file_name
+from utils import read_info, make_peaks_file_name
 
-# %% Read info file
-info = read_info()
-core_df = pd.read_csv('results/core_df.csv')
-subject_list = np.unique(core_df['subject_idx'])
-experiments = info['experiments']
-methods_names = np.array(info['methods_names'])
-methods_names = methods_names[methods_names != 'Engzee']
-setup = 'einthoven'
-FS = 250  # This should be inside the info file
-read_path = Path('results/rr_detection')
-save_path = Path('results/HRV')
 
-# %% Open detected peaks file
-df_det_hrv = pd.DataFrame()
-df_ann_hrv = pd.DataFrame()
+def compute_hrv(setup, methods_names):
+    df_det_hrv = pd.DataFrame()
+    df_ann_hrv = pd.DataFrame()
 
-for s in tqdm(subject_list, desc='Subject'):
-    for experiment in tqdm(experiments, desc='Condition'):
+    for s in tqdm(subject_list, desc='Subject'):
+        for experiment in tqdm(experiments, desc='Condition'):
 
-        ann_file = read_path / make_peaks_file_name(s, setup, experiment, 'annotated')
-        try:
-            annotated_peaks = np.load(ann_file)
-        except FileNotFoundError:
-            tqdm.write(f'Skip subject {s}, experiemnt {experiment}.')
-            core_df = core_df[np.logical_and(core_df.subject_idx !=
-                              s, core_df.experiment != experiment)]
-            continue
-
-        # Compute HRV
-        ann_hrv = nk.hrv(annotated_peaks, FS)
-        ann_hrv['method'] = 'Annotated'
-        ann_hrv['experiment'] = experiment
-        ann_hrv['subject_idx'] = s
-
-        df_ann_hrv = pd.concat([df_ann_hrv, ann_hrv])
-
-        for method in tqdm(methods_names, desc='Method', leave=False):
-
-            det_file = read_path / make_peaks_file_name(s, setup, experiment, method)
+            ann_file = read_path / make_peaks_file_name(s, setup, experiment, 'annotated')
             try:
-                detected_peaks = np.load(det_file)
+                annotated_peaks = np.load(ann_file)
             except FileNotFoundError:
-                tqdm.write(f'skip subject {s}: {method}')
-                # core_df = core_df[core_df.subject_idx !=
-                #                  s and core_df.method != method]
+                tqdm.write(f'Skip subject {s}, experiemnt {experiment}.')
                 continue
 
-            det_hrv = nk.hrv(detected_peaks, FS)
-            det_hrv['method'] = method
-            det_hrv['experiment'] = experiment
-            det_hrv['subject_idx'] = s
+            # Compute HRV
+            ann_hrv = nk.hrv(annotated_peaks, FS)
+            ann_hrv['method'] = 'Annotated'
+            ann_hrv['experiment'] = experiment
+            ann_hrv['subject_idx'] = s
 
-            df_det_hrv = pd.concat([df_det_hrv, det_hrv])
+            df_ann_hrv = pd.concat([df_ann_hrv, ann_hrv])
 
-df_det_hrv = pd.concat([df_det_hrv, df_ann_hrv], axis=0)
-df_det_hrv = df_det_hrv.set_index(np.arange(len(df_det_hrv)))
-df_det_hrv.to_csv(save_path / f'{setup}_HRV_results.csv')
+            for method in tqdm(methods_names, desc='Method', leave=False):
+                det_file = read_path / make_peaks_file_name(s, setup, experiment, method)
+                try:
+                    detected_peaks = np.load(det_file)
+                except FileNotFoundError:
+                    tqdm.write(f'skip subject {s}: {method}')
+                    continue
+                try:
+                    det_hrv = nk.hrv(detected_peaks, FS)
+                    det_hrv['method'] = method
+                    det_hrv['experiment'] = experiment
+                    det_hrv['subject_idx'] = s
+                except (ValueError, ZeroDivisionError):
+                    tqdm.write(f'Error computing HRV for subject {s}, '
+                               f'condition {experiment}, method {method}')
+                else:
+                    df_det_hrv = pd.concat([df_det_hrv, det_hrv])
+
+    df_det_hrv = pd.concat([df_det_hrv, df_ann_hrv], axis=0)
+    df_det_hrv = df_det_hrv.set_index(np.arange(len(df_det_hrv)))
+    fn = save_path / f'{setup}_HRV_results.csv'
+    df_det_hrv.to_csv(fn)
+    print('Results saved in', fn)
+
+
+if __name__ == '__main__':
+    info = read_info()
+    core_df = pd.read_csv('results/core_df.csv')
+    subject_list = np.unique(core_df['subject_idx'])
+    experiments = info['experiments']
+    methods_names = np.array(info['methods_names'])
+    methods_names = methods_names[methods_names != 'Engzee']
+    FS = 250  # This should be inside the info file
+    read_path = Path('results/rr_detection')
+    save_path = Path('results/HRV')
+    compute_hrv('einthoven', methods_names)
+    methods_names = np.array(info['methods_names'])
+    compute_hrv('chest_strap', methods_names)
