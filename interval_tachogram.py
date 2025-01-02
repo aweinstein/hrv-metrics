36c9a038175abd88186d312ef9a6d4cee8287808
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2025-01-02 13:59:40 -0300

    Refactor interval_tachogram.py.
    Remove unnecessary code and transform code into function with setup as parameter.

--- interval_tachogram.py
+++ interval_tachogram.py
@@ -1,110 +1,65 @@
-"""Plot interval tachogram for each subject and method. Also for each condition."""
-# %% Load libraries
+"""
+Plot interval tachogram for each subject, method, and condition.
+"""
+from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 import pandas as pd
-from utils import read_info
-from utils import save_figs_as_pdf
+from utils import read_info, save_figs_as_pdf, make_peaks_file_name
 from tqdm import tqdm
 
-
-# %% Read info file
+results_path = Path(__file__).resolve().parent /  'results'
 info = read_info()
-core_df = pd.read_csv('results/core_df.csv')
+core_df = pd.read_csv(results_path / 'core_df.csv')
 subject_list = np.unique(core_df['subject_idx'])
 experiments = info['experiments']
 methods_names = info['methods_names']
 
 FS = 250  # This should be inside the info file
-READ_PATH = 'results/rr_detection/'
-SAVE_PATH = 'results/tachograms'
+read_path = results_path / 'rr_detection'
 
-# %% Iterate through subjects
-for i, s in enumerate(tqdm(subject_list)):
 
-    # Iterate through experiments
-    for ii, experiment in enumerate(experiments):
-        # Load annotated peaks
-        ann_file = f'{READ_PATH}/s[{s}]/{experiment}/annotated_peaks.npy'
-
-        # Check if file exists
-        if os.path.exists(ann_file):
-            annotated_peaks = np.load(ann_file)
-        else:
-            print(f'skip subject {s}, Exp: {experiment}...')
-            core_df = core_df[np.logical_and(core_df.subject_idx !=
-                                             s, core_df.experiment != experiment)]
-            continue
-
-        # Compute tachogram of annotated peaks
-        ann_tachogram = np.diff(annotated_peaks) / FS
-
-        # Iterate through methods
-        figs = []
-        for iii, method in enumerate(methods_names):
-
-            save_path = f'{SAVE_PATH}/s[{s}]/{experiment}/{method}'
-
-            detected_peaks = np.load(
-                f'{READ_PATH}/s[{s}]/{experiment}/{method}/detected_peaks.npy')
-            det_tachogram = np.diff(detected_peaks) / FS
-
-            bins = np.linspace(np.amin(ann_tachogram),
-                               np.amax(ann_tachogram), 50)
-
-            fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
-
-            axs[0].hist(det_tachogram, bins=bins, color='purple')
-
-            axs[0].set_title('Subject: ' + str(s) + ' - ' + experiment +
-                             ' - ' + method)
-
-            axs[1].hist(ann_tachogram, bins=bins, color='teal')
-            axs[1].set_title('Annotated Tachogram')
-            axs[1].set_xlabel('RR interval (s)')
-
+def plot_tachograms(setup):
+    figs = []
+    for s in tqdm(subject_list, desc='Subject'):
+        for experiment in tqdm(experiments, desc='Condition'):
+            # Load annotated peaks
+            ann_file = read_path / make_peaks_file_name(s, setup, experiment, 'annotated')
             try:
-                plt.savefig(f'{save_path}/detected_peaks.png')
+                annotated_peaks = np.load(ann_file)
             except FileNotFoundError:
-                os.makedirs(save_path)
-                plt.savefig(f'{save_path}/detected_peaks.png')
+                tqdm.write(f'Skip subject {s}, experiemnt {experiment}.')
+                continue
 
-            figs.append(fig)
+            # Compute tachogram of annotated peaks
+            ann_tachogram = np.diff(annotated_peaks) / FS
 
-        save_figs_as_pdf(
-            figs, f'{SAVE_PATH}/s[{s}]_{experiment}_tachogram.pdf')
+            for method in tqdm(methods_names, desc='Method', leave=False):
+                det_file = read_path / make_peaks_file_name(s, setup, experiment, method)
+                try:
+                    detected_peaks = np.load(det_file)
+                except FileNotFoundError:
+                    tqdm.write(f'skip subject {s}: {method}')
+                    continue
+                det_tachogram = np.diff(detected_peaks) / FS
+                bins = np.linspace(np.amin(ann_tachogram),
+                                   np.amax(ann_tachogram), 50)
 
-        # Clear memory
-        del figs
-        plt.close('all')
+                fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
+                axs[0].hist(det_tachogram, bins=bins, color='purple')
+                title = f'Setup {setup}, subject {s}, condition {experiment}, ' \
+                    f'method {method}'
+                axs[0].set_title(title)
 
-# %% Plot tachogram for a given subject and condition
+                axs[1].hist(ann_tachogram, bins=bins, color='teal')
+                axs[1].set_title('Annotated Tachogram')
+                axs[1].set_xlabel('RR interval (s)')
+                plt.close(fig)
+                figs.append(fig)
 
-s = 11
-method = 'Engzee'
-experiment = 'walking'
+    save_figs_as_pdf(figs, results_path /f'tachogram_{setup}.pdf')
 
-ann_file = f'{READ_PATH}/s[{s}]/{experiment}/annotated_peaks.npy'
-annotated_peaks = np.load(ann_file)
 
-det_file = f'{READ_PATH}/s[{s}]/{experiment}/{method}/detected_peaks.npy'
-detected_peaks = np.load(det_file)
-
-ann_tachogram = np.diff(annotated_peaks) / FS
-det_tachogram = np.diff(detected_peaks) / FS
-
-# %% Plot histogram with subplots
-
-fig, axs = plt.subplots(2, 1)
-
-axs[0].hist(det_tachogram, color='purple')
-axs[0].set_title('Subject: ' + str(s) + ' - ' + experiment + ' - ' + method)
-
-axs[1].hist(ann_tachogram, color='teal')
-
-axs[1].set_title('Annotated Tachogram')
-axs[1].set_xlabel('RR interval (s)')
-
-plt.tight_layout()
-plt.show()
+if __name__ == '__main__':
+    plot_tachograms('einthoven')
+    plot_tachograms('chest_strap')
