08b4282796f49599dcfe8bfc100a7d6cd9f9b140
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2025-04-01 05:23:43 -0300

    Improve readability of figure 3.

--- vis_eeg.py
+++ vis_eeg.py
@@ -45,7 +45,7 @@ def plot_single_case(subject, experiment, setup, methods):
     plt.close('all')
     _, ax = plt.subplots(figsize=(6, 4))
     ax.plot(time, data, color='black', lw=0.5, label='ECG')
-    ax.set_prop_cycle(marker=['+', 'x', 'o', 'D', '1', '2', '3'],
+    ax.set_prop_cycle(marker=['+', 'o', 'x', 'D', '1', '2', '3'],
                       color=['#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b',
                              '#e377c2', '#7f7f7f'])
     ms = 7
